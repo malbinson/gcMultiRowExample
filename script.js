1e7fe0d75583b57c4c8124c48167386ab721138e
malbinson <mattalbinson@berkeley.net>
2018-10-25 10:35:46 -0700

    ss

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-var rowCount = 0;
+var rowCount = 1;
+
+function createTable() {
+
+    var div = document.getElementById('tableContainer');
+    var table = document.createElement("table");
+    table.setAttribute("id","mainTable");
+    div.appendChild(table);
+
+    addRow();
+}
 
 function addRow() {
 
-    var table = document.getElementById('primary');
+    var table = document.getElementById("mainTable");
     var fieldRow = document.createElement("tr");
     var labelRow = document.createElement("tr");
 
@@ -12,11 +22,34 @@ function addRow() {
     var td4 = document.createElement("td");
     var i1 = document.createElement("input");
     var i2 = document.createElement("input");
-    i1.setAttribute("id","id1");
-    i2.setAttribute("id","id2");
 
-    var c = "red";
+    var label = document.getElementById("catName").value;
+    i1.setAttribute("id","pts" + label);
+    i2.setAttribute("id","wgt" + label);
 
+    if(label.length==0){
+        label = "Homework";
+    }
+
+    td1.innerHTML = label + " Points";
+    td2.innerHTML = label + " Weight";
+
+    td3.append(i1);
+    td4.append(i2);
+    labelRow.append(td1);
+    labelRow.append(td2);
+    fieldRow.append(td3);
+    fieldRow.append(td4);
+    table.append(labelRow);
+    table.append(fieldRow);
+
+    rowCount++;
+
+    colorRows(fieldRow, labelRow);
+
+}
+
+function colorRows(f,l) {
     if(rowCount % 3 ==0) {
         c = "red";
     }
@@ -27,33 +60,7 @@ function addRow() {
         c = "gray"
     }
 
-    td1.setAttribute("class",c);
+    f.setAttribute("class",c);
+    l.setAttribute("class",c);
 
-    var label = document.getElementById("catName").value;
-    console.log(label.length);
-    if(label.length==0){
-        label = "Homework";
-    }
-
-    td1.innerHTML = label + " Points";
-    td2.innerHTML = label + " Weight";
-    td3.appendChild(i1);
-    td4.appendChild(i2);
-
-    labelRow.append(td1);
-    labelRow.append(td2);
-
-    fieldRow.append(td3);
-    fieldRow.append(td4);
-
-    table.append(labelRow);
-    table.append(fieldRow);
-
-    rowCount++;
-
-}
-
-function setUpPage() {
-    addRow();
-    document.getElementById("container").style.background = "pink";
 }
